1e1c8c35bf3fa7ec0b208f1671dcaadb5ef8ccdf
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-08-06 10:40:38 +0900

    chore(deps): more api change

--- src/cli.ts
+++ src/cli.ts
@@ -17,8 +17,8 @@ import { version } from "../package.json";
 import { DEFAULT_OPTIONS, IsortOptions } from "./misc";
 import { IsortError, tsTransformIsort } from "./transformer";
 
-const flag = (help?: string): ArgSchema<boolean> => ({
-  help,
+const flag = (description?: string): ArgSchema<boolean> => ({
+  description,
   type: "flag",
   parse: (v: unknown) => Boolean(v),
 });
@@ -27,7 +27,7 @@ const argsSchema = {
   files: {
     type: "positional",
     variadic: true,
-    help: "typescript files",
+    description: "typescript files",
     parse: (v: unknown) => v as string[],
   },
   fix: flag("apply sorting in-place"),
@@ -42,7 +42,7 @@ const argsSchema = {
 const command = defineCommand(
   {
     program: "isort-ts",
-    help: "Lint ESM module import order",
+    description: "Lint ESM module import order",
     autoHelp: true,
     args: argsSchema,
   },
